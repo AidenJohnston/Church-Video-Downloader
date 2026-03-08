0d267cba8271da5117824a8cab25bd9b61fb8baf
AidenJohnston <aidendj@icloud.com>
2026-03-08 09:47:47 -0500

    took the tv instance out of the opts

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ if st.button("Download", type="primary"):
                     "merge_output_format": "mp4",
                     "extractor_args": {
                         "youtube": {
-                            "player_client": ["tv", "web_embedded", "web"],
+                            "player_client": ["web", "web_embedded"],
                         }
                     },
                     "http_headers": {
